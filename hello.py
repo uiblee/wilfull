ccfcc3c40c62c5f8c8d693d83c34131de4d005c7
Elisabeth Uible <uiblee@Elisabeths-MacBook-Air.local>
2015-11-11 08:00:48 -0500

    bootstrap

--- hello.py
+++ hello.py
@@ -4,6 +4,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-	return "Hello World!"
+def homepage():
+	try:
+		return "Hello World!"
+	except: Exception, e:
+		return str(e)
 
+if __name__ == "__main__"
+	app.run()
